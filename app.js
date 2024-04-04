8687a367c9c6ff31024bdf33b9e28ed513945064
maamirkh <maamirkhan1983@gmail.com>
2024-04-05 00:47:43 +0500

    updated

---
+++ app.js
@@ -0,0 +1,23 @@
+#! usr/bin/env node
+let favorite_fruits = ["apples", "bananas", "cherries", "mangoes", "melons"];
+if (favorite_fruits.includes("bananas")) {
+    console.log("You really like bananas!");
+}
+if (favorite_fruits.includes("apples")) {
+    console.log("You really like apples!");
+}
+if (favorite_fruits.includes("cherries")) {
+    console.log("You really like cherries!");
+}
+if (favorite_fruits.includes("mangoes")) {
+    console.log("You really like mangoes!");
+}
+if (favorite_fruits.includes("melons")) {
+    console.log("You really like melons!");
+}
+if (favorite_fruits.includes("kiwis")) {
+    console.log("You really like kiwis!");
+}
+else
+    console.log("You don't like kiwis!");
+export {};
